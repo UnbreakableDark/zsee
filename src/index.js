be9d51683cdb0bd4fa470bde25d56a2972ad577d
UnbreakableDark <a2452590730@163.com>
2023-02-12 21:55:29 +0800

    开始学习

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react';//核心包
+import ReactDOM from 'react-dom/client';//专门做渲染相关的包
+import './index.css';//全局样式文件
+import App from './App';//根组件
 
+
+//渲染根组件
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
+
